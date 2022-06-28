1721035ab1f5da599ff53fe495781119ddb05d3f
Rune Tynan <runetynan@gmail.com>
2022-06-28 13:36:18 -0400

    Add stable/unstable for name change

--- src/structs.rs
+++ src/structs.rs
@@ -465,6 +465,7 @@ impl Default for i386_avx_state_t {
 
 #[repr(C)]
 #[derive(Copy, Clone, Debug, Default, Hash, PartialOrd, PartialEq, Eq, Ord)]
+#[cfg(feature = "unstable")]
 pub struct x86_thread_state64_t {
     pub rax: u64,
     pub rbx: u64,
@@ -489,9 +490,37 @@ pub struct x86_thread_state64_t {
     pub gs: u64,
 }
 
+#[repr(C)]
+#[derive(Copy, Clone, Debug, Default, Hash, PartialOrd, PartialEq, Eq, Ord)]
+#[cfg(not(feature = "unstable"))]
+pub struct x86_thread_state64_t {
+    pub __rax: u64,
+    pub __rbx: u64,
+    pub __rcx: u64,
+    pub __rdx: u64,
+    pub __rdi: u64,
+    pub __rsi: u64,
+    pub __rbp: u64,
+    pub __rsp: u64,
+    pub __r8: u64,
+    pub __r9: u64,
+    pub __r10: u64,
+    pub __r11: u64,
+    pub __r12: u64,
+    pub __r13: u64,
+    pub __r14: u64,
+    pub __r15: u64,
+    pub __rip: u64,
+    pub __rflags: u64,
+    pub __cs: u64,
+    pub __fs: u64,
+    pub __gs: u64,
+}
+
 impl x86_thread_state64_t {
     pub fn new() -> Self {
-        Self {
+        #[cfg(feature = "unstable")]
+        return Self {
             rax: 0,
             rbx: 0,
             rcx: 0,
@@ -513,7 +542,31 @@ impl x86_thread_state64_t {
             cs: 0,
             fs: 0,
             gs: 0,
-        }
+        };
+        #[cfg(not(feature = "unstable"))]return Self {
+            __rax: 0,
+            __rbx: 0,
+            __rcx: 0,
+            __rdx: 0,
+            __rdi: 0,
+            __rsi: 0,
+            __rbp: 0,
+            __rsp: 0,
+            __r8: 0,
+            __r9: 0,
+            __r10: 0,
+            __r11: 0,
+            __r12: 0,
+            __r13: 0,
+            __r14: 0,
+            __r15: 0,
+            __rip: 0,
+            __rflags: 0,
+            __cs: 0,
+            __fs: 0,
+            __gs: 0,
+        };
+
     }
 
     pub fn count() -> mach_msg_type_number_t {
