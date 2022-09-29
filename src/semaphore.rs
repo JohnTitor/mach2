0fc82f020dfebe2529eaa23f4fa1f123414be40d
DoumanAsh <douman@gmx.se>
2022-09-29 21:39:41 +0900

    Do not use crate::

--- src/semaphore.rs
+++ src/semaphore.rs
@@ -1,9 +1,9 @@
 //! This module corresponds to `mach/semaphore.h`
 
-use crate::clock_types::mach_timespec_t;
-use crate::kern_return::kern_return_t;
-use crate::mach_types::{semaphore_t, task_t};
-use crate::sync_policy::sync_policy_t;
+use clock_types::mach_timespec_t;
+use kern_return::kern_return_t;
+use mach_types::{semaphore_t, task_t};
+use sync_policy::sync_policy_t;
 
 extern "C" {
     pub fn semaphore_create(
