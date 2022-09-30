0b1acaa5372b5c2ad453844661e84975846002e4
DoumanAsh <douman@gmx.se>
2022-09-30 10:08:28 +0900

    Correct semaphore argument signature

--- src/semaphore.rs
+++ src/semaphore.rs
@@ -8,12 +8,12 @@ use sync_policy::sync_policy_t;
 extern "C" {
     pub fn semaphore_create(
         task: task_t,
-        semaphore: semaphore_t,
+        semaphore: *mut semaphore_t,
         policy: sync_policy_t,
         value: libc::c_int,
     ) -> kern_return_t;
-    pub fn semaphore_signal(semaphore: semaphore_t) -> kern_return_t;
-    pub fn semaphore_wait(semaphore: semaphore_t) -> kern_return_t;
-    pub fn semaphore_timedwait(semaphore: semaphore_t, timeout: mach_timespec_t) -> kern_return_t;
-    pub fn semaphore_destroy(task: task_t, semaphore: semaphore_t) -> kern_return_t;
+    pub fn semaphore_signal(semaphore: *mut semaphore_t) -> kern_return_t;
+    pub fn semaphore_wait(semaphore: *mut semaphore_t) -> kern_return_t;
+    pub fn semaphore_timedwait(semaphore: *mut semaphore_t, timeout: mach_timespec_t) -> kern_return_t;
+    pub fn semaphore_destroy(task: task_t, semaphore: *mut semaphore_t) -> kern_return_t;
 }
