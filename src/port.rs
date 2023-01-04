aab46bbc2c492a06745f3365dee1728f3237fdc7
turbocooler <turbocooler@cocaine.ninja>
2023-01-04 22:25:27 +0300

    Add mach_port_construct and the required types.

--- src/port.rs
+++ src/port.rs
@@ -32,3 +32,35 @@ pub type mach_port_seqno_t = natural_t;
 pub type mach_port_mscount_t = natural_t;
 pub type mach_port_msgcount_t = natural_t;
 pub type mach_port_rights_t = natural_t;
+
+pub const MACH_PORT_QLIMIT_ZERO: mach_port_msgcount_t = 0;
+pub const MACH_PORT_QLIMIT_BASIC: mach_port_msgcount_t = 5;
+pub const MACH_PORT_QLIMIT_SMALL: mach_port_msgcount_t = 16;
+pub const MACH_PORT_QLIMIT_LARGE: mach_port_msgcount_t = 1024;
+pub const MACH_PORT_QLIMIT_KERNEL: mach_port_msgcount_t = 65534;
+pub const MACH_PORT_QLIMIT_MIN: mach_port_msgcount_t = MACH_PORT_QLIMIT_ZERO;
+pub const MACH_PORT_QLIMIT_DEFAULT: mach_port_msgcount_t = MACH_PORT_QLIMIT_BASIC;
+pub const MACH_PORT_QLIMIT_MAX: mach_port_msgcount_t = MACH_PORT_QLIMIT_LARGE;
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug, Default, Hash, PartialOrd, PartialEq, Eq, Ord)]
+pub struct mach_port_limits_t {
+    pub mpl_qlimit: mach_port_msgcount_t,
+}
+
+pub const MPO_CONTEXT_AS_GUARD: u32 = 1;
+pub const MPO_QLIMIT: u32 = 2;
+pub const MPO_TEMPOWNER: u32 = 4;
+pub const MPO_IMPORTANCE_RECEIVER: u32 = 8;
+pub const MPO_INSERT_SEND_RIGHT: u32 = 0x10;
+pub const MPO_STRICT: u32 = 0x20;
+pub const MPO_DENAP_RECEIVER: u32 = 0x40;
+pub const MPO_IMMOVABLE_RECEIVE: u32 = 0x80;
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug, Default, Hash, PartialOrd, PartialEq, Eq, Ord)]
+pub struct mach_port_options_t {
+    pub flags: u32,
+    pub mpl: mach_port_limits_t,
+    pub reserved: [u64; 2],
+}
