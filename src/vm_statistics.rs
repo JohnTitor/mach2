05fdd728125334640aee35758302a119802c3029
ldm0 <liudingming@bytedance.com>
2025-08-01 02:59:32 +0800

    feat(vm_statistics): add new vm statistics types and structs
    Add vm_statistics64, vm_extmod_statistics, vm_purgeable_stat and vm_purgeable_info types to support extended virtual memory statistics reporting. These additions provide more detailed memory management metrics for monitoring and analysis.

--- src/vm_statistics.rs
+++ src/vm_statistics.rs
@@ -4,6 +4,12 @@ use vm_types::{integer_t, natural_t};
 
 pub type vm_statistics_t = *mut vm_statistics;
 pub type vm_statistics_data_t = vm_statistics;
+pub type vm_statistics64_t = *mut vm_statistics64;
+pub type vm_statistics64_data_t = vm_statistics64;
+pub type vm_extmod_statistics_t = *mut vm_extmod_statistics;
+pub type vm_extmod_statistics_data_t = vm_extmod_statistics;
+pub type vm_purgeable_stat_t = vm_purgeable_stat;
+pub type vm_purgeable_info_t = *mut vm_purgeable_info;
 
 pub const VM_PAGE_QUERY_PAGE_PRESENT: integer_t = 1;
 pub const VM_PAGE_QUERY_PAGE_FICTITIOUS: integer_t = 1 << 1;
@@ -55,4 +61,59 @@ pub struct vm_statistics {
     pub purgeable_count: natural_t,
     pub purges: natural_t,
     pub speculative_count: natural_t,
+}
+
+#[repr(C, packed(8))]
+#[derive(Copy, Clone, Debug, Default, Hash, PartialOrd, PartialEq, Eq, Ord)]
+pub struct vm_statistics64 {
+    pub free_count: natural_t,
+    pub active_count: natural_t,
+    pub inactive_count: natural_t,
+    pub wire_count: natural_t,
+    pub zero_fill_count: u64,
+    pub reactivations: u64,
+    pub pageins: u64,
+    pub pageouts: u64,
+    pub faults: u64,
+    pub cow_faults: u64,
+    pub lookups: u64,
+    pub hits: u64,
+    pub purges: u64,
+    pub purgeable_count: natural_t,
+    pub speculative_count: natural_t,
+    pub decompressions: u64,
+    pub compressions: u64,
+    pub swapins: u64,
+    pub swapouts: u64,
+    pub compressor_page_count: natural_t,
+    pub throttled_count: natural_t,
+    pub external_page_count: natural_t,
+    pub internal_page_count: natural_t,
+    pub total_uncompressed_pages_in_compressor: u64,
+}
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug, Default, Hash, PartialOrd, PartialEq, Eq, Ord)]
+pub struct vm_extmod_statistics {
+    pub task_for_pid_count: i64,
+    pub task_for_pid_caller_count: i64,
+    pub thread_creation_count: i64,
+    pub thread_creation_caller_count: i64,
+    pub thread_set_state_count: i64,
+    pub thread_set_state_caller_count: i64,
+}
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug, Default, Hash, PartialOrd, PartialEq, Eq, Ord)]
+pub struct vm_purgeable_stat {
+    pub count: u64,
+    pub size: u64,
+}
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug, Default, Hash, PartialOrd, PartialEq, Eq, Ord)]
+pub struct vm_purgeable_info {
+    pub fifo_data: [vm_purgeable_stat_t; 8usize],
+    pub obsolete_data: vm_purgeable_stat_t,
+    pub lifo_data: [vm_purgeable_stat_t; 8usize],
 }
